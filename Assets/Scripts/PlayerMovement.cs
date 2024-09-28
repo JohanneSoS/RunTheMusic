32a43c29baeaab44d8451e93493310ce68891116
JohanneSos <johannes@soundofsynergy.com>
2024-09-28 15:10:33 +0200

    Implement Dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,9 +13,16 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float jumpingPower = 10f;
     [SerializeField] private bool isFacingRight = true;
 
+    private bool canDash = true;
+    private bool isDashing;
+    [SerializeField] private float dashingPower = 24f;
+    [SerializeField] private float dashingTime = 0.2f;
+    [SerializeField] private float dashingCooldown = 1f;
+
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private TrailRenderer tr;
 
     public LevelScript levelScript;
     public Blackscreen blackScreenScript;
@@ -34,6 +41,10 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        if (isDashing)
+        {
+            return;
+        }
         
         horizontal = Input.GetAxisRaw("Horizontal");
 
@@ -45,6 +56,11 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
+        {
+            StartCoroutine(Dash());
         }
         
         Flip();
@@ -145,6 +161,11 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (isDashing)
+        {
+            return;
+        }
+        
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
@@ -162,6 +183,22 @@ public class PlayerMovement : MonoBehaviour
             localeScale.x *= -1f;
             transform.localScale = localeScale;
         }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
